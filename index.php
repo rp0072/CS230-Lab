dfdb8fafab24ffb1e783f9aa170d8d914da6f92f
rp0072 <rp0072@mix.wvu.edu>
2021-02-05 18:15:42 -0500

    Lab 2 Char's Cars

---
+++ index.php
@@ -0,0 +1,54 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>Char's Cars</h1>
+        <p>Information on all your favorite cars</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/eg_civic.jpeg">
+                <div class="container">
+                    <h2>Eg Honda Civic si Hatchback</h2>
+                    <p>125 hp</p>
+                    <p>0-60mph: 7.5 s</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/AE86.jpg">
+                <div class="container">
+                    <h2>Toyota Corolla AE86 Trueno</h2>
+                    <p>130 hp</p>
+                    <p>0-60mph: 8.6 s</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/rx7.jpg">
+                <div class="container">
+                    <h2>Mazda RX7 Fd Sprit R</h2>
+                    <p>255 hp</p>
+                    <p>0-60mph: 4.9 s</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/r34gtr.jpg">
+                <div class="container">
+                    <h2>Nissan Skyline GTR R34 M Spec Nur</h2>
+                    <p>276 hp</p>
+                    <p>0-60mph: 4.6 s</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
