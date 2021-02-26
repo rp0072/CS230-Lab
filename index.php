db26c578dec85d193223e9492b15c5fe9c5632b9
rp0072 <rp0072@mix.wvu.edu>
2021-02-26 14:22:05 -0500

    Lab5

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
+<?php
+require 'includes/header.php';
+?>
 <main>
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
-        <h1>Char's Cars</h1>
-        <p>Information on all your favorite cars</p>
+        <h1>About us</h1>
+        <p>The Real OG's</p>
     </div>
     <div class="row">
         <div class="column">
             <div class="card">
                 <img src="images/eg_civic.jpeg">
                 <div class="container">
-                    <h2>Eg Honda Civic si Hatchback</h2>
+                    <h3>Eg Honda Civic si Hatchback</h3>
                     <p>125 hp</p>
                     <p>0-60mph: 7.5 s</p>
 
@@ -21,7 +24,7 @@
             <div class="card">
                 <img src="images/AE86.jpg">
                 <div class="container">
-                    <h2>Toyota Corolla AE86 Trueno</h2>
+                    <h3>Toyota Corolla AE86 Trueno</h3>
                     <p>130 hp</p>
                     <p>0-60mph: 8.6 s</p>
 
@@ -32,7 +35,7 @@
             <div class="card">
                 <img src="images/rx7.jpg">
                 <div class="container">
-                    <h2>Mazda RX7 Fd Sprit R</h2>
+                    <h3>Mazda RX7 Fd Sprit R</h3>
                     <p>255 hp</p>
                     <p>0-60mph: 4.9 s</p>
 
@@ -43,7 +46,7 @@
             <div class="card">
                 <img src="images/r34gtr.jpg">
                 <div class="container">
-                    <h2>Nissan Skyline GTR R34 M Spec Nur</h2>
+                    <h3>Nissan Skyline GTR R34 M Spec Nur</h3>
                     <p>276 hp</p>
                     <p>0-60mph: 4.6 s</p>
 
